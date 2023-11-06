058db37370124f5a36e29e12a9119bcc754a8818
Asitulom <asieriturriotz@opendeusto.es>
2023-11-06 14:06:44 +0100

    un nuevo comienzo 2

--- top100/appTop100/urls.py
+++ top100/appTop100/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+
 from . import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.index_departamentos, name='index'),
-    path('departamentos/<int:departamento_id>/', views.show_departamento, name='detail'),
-    path('departamentos/<int:departamento_id>/empleados', views.index_empleados, name='empleados'),
-    path('empleados/<int:empleado_id>', views.show_empleado, name='empleado'),
-    path('habilidades/<int:habilidad_id>', views.show_habilidad, name='habilidad'),
+ path('departamento/<int:departamento_id>', views.detail, name='detail'),
+ path('departamento/<int:departamento_id>/empleados', views.empleados, name='empleados'),
+ path('empleado/<int:empleado_id>', views.empleado, name='empleado'),
+ path('habilidad/<int:habilidad_id>', views.habilidad, name='habilidad')
 ]
