d53995727db754a6f5302a700cd281efd3397547
Jon Ander Gallastegui <gallastegi.jonander@opendeusto.es>
2023-11-07 18:34:48 +0100

    funciona

--- top100/appTop100/urls.py
+++ top100/appTop100/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.index, name='index'),
+    path('', views.index_estilos, name='index'),
+    path('estilos/<int:estilo_id>/', views.show_estilos, name='detail'),
+    path('estilos/<int:estilo_id>/canciones', views.index_canciones, name='canciones'),
+    path('canciones/<int:cancion_id>', views.show_cancion, name='cancion'),
+    path('interpretes/<int:interprete_id>', views.show_interpretes, name='interprete'),
 ]
--- top100/appTop100/views.py
+++ top100/appTop100/views.py
@@ -1,37 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 from .models import Estilo, Interprete, Cancion
-
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 #devuelve el listado de estilos
 def index_estilos(request):
-	estilos = Estilo.objects.order_by('nombre')
-	output = ', '.join([e.nombre for e in estilos])
-	return HttpResponse(output)
+        estilos  = get_list_or_404(Estilo.objects.order_by('nombre'))
+        context = {'lista_estilos': estilos }
+        return render(request, 'index.html', context)
 
 #devuelve los datos de un estilo
 def show_estilos(request, estilo_id):
-	estilo = Estilo.objects.get(pk=estilo_id)
-	output = f'Detalles del estilo: {estilo.id}, {estilo.nombre}'
-	return HttpResponse(output)
+        estilo = get_object_or_404(Estilo, pk=estilo_id)
+        context = {'estilo': estilo }
+        return render(request, 'detail.html', context)
 
 #devuelve los detalles de un interprete
 def show_interpretes(request, interprete_id):
-	interprete = Interprete.objects.get(pk=interprete_id)
-	output = f'Detalles del Interprete: {interprete.id}, {interprete.nombre}, {interprete.fecha_nacimiento}. Canciones: {[c.nombre for c in interprete.cancion_set.all()]}'
-	return HttpResponse(output)
-
+        interprete = get_object_or_404(Interprete, pk=interprete_id)
+        canciones =  interprete.cancion_set.all()
+        context = { 'canciones': canciones, 'interprete' : interprete }
+        return render(request, 'interprete.html', context)
 
 #devuelve los detalles de una cancion
 def index_canciones(request, estilo_id):
-        estilo = Estilo.objects.get(pk=estilo_id)
-        output = ', '.join([c.titulo for c in estilo.cancion_set.all()])  
-        return HttpResponse(output)
+        estilo = get_object_or_404(Estilo, pk=estilo_id)
+        canciones =  estilo.cancion_set.all()
+        context = {'estilo': estilo, 'canciones' : canciones }
+        return render(request, 'canciones.html', context)
 
 #devuelve los detalles de una habilidad
 def show_cancion(request, cancion_id):
-	cancion = Cancion.objects.get(pk=cancion_id)
-	output = f'Detalles de la cancion: {cancion.id}, {cancion.titulo}, {str(cancion.estilo)}. Interpretes: {[i.nombre for i in cancion.interpretes.all()]}'
-	return HttpResponse(output)
+        cancion = get_object_or_404(Cancion, pk=cancion_id)
+        interpretes = cancion.interpretes.all()
+        context = { 'cancion': cancion, 'interpretes' : interpretes }
+        return render(request, 'cancion.html', context)
