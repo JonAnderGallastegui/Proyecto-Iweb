3f2e7959ab4317d8c28821c8400ba11647a5dbab
Asitulom <asieriturriotz@opendeusto.es>
2023-12-04 14:52:31 +0100

    formulario va mal

--- formas/appFormularios/urls.py
+++ formas/appFormularios/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('registro/', views.show_form, name='registro')
+    path('registro/', views.show_form, name='registro'),
+    path('registrar/', views.show_form, name='registrar')
 ]
--- formas/appFormularios/views.py
+++ formas/appFormularios/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def show_form(request):
     return render(request, 'registro.html')
-# Create your views here.
+
+def show_form(request):
+    usuario = request.POST["usuario"]
+    email = request.POST["email"]
+    return HttpResponse(f"El usuario es {usuario} y el email {email}")
+
