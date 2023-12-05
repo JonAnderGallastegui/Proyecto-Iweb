7b49930eb6cc914c8315eb299b6d3ac9a1628d0b
Asitulom <asieriturriotz@opendeusto.es>
2023-12-05 12:53:58 +0100

    formulario 1/3 terminado

--- formas/appFormularios/urls.py
+++ formas/appFormularios/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('registro/', views.show_form, name='registro'),
-    path('registrar/', views.show_form, name='registrar')
+    path('registrar/', views.post_form, name='registrar')
 ]
--- formas/appFormularios/views.py
+++ formas/appFormularios/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def show_form(request):
     return render(request, 'registro.html')
 
-def show_form(request):
+def post_form(request):
     usuario = request.POST["usuario"]
     email = request.POST["email"]
     return HttpResponse(f"El usuario es {usuario} y el email {email}")
