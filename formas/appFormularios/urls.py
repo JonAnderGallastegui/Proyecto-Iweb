8576fb6e59786a6140789a14ee1d95bf6ab60be4
Jon Ander Gallastegui <gallastegi.jonander@opendeusto.es>
2023-12-07 20:55:54 +0100

    agenda vue no funciona bien

--- formas/appFormularios/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('registro/', views.show_form, name='registro'),
-    path('registrar/', views.post_form, name='registrar')
-]
